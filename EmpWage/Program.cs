0a90fe7e7a9efd5ebffaa3defb7df6c0849bd1ca
unknown <hindhujhasridhar@gmail.com>
2021-11-25 20:20:30 +0530

    EMPLOYEE WAGE PROGRAMS

---
+++ EmpWage/Program.cs
@@ -0,0 +1,12 @@
+﻿Using System;
+namespace EmpWage
+{
+    class UC0
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE PROGRAMS");
+        }
+    }
+
+}
