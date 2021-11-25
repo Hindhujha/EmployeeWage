4b1dd7eada68115f0bbe12a7b464e5bf5355e91c
unknown <hindhujhasridhar@gmail.com>
2021-11-25 21:44:26 +0530

    Daily Wage Calculated

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,24 +1,28 @@
-﻿//check employee is present or absent
-
-using System;
-namespace EmpWage
+﻿//Employee Wage added
+    using System;
+namespace EmpWage2
 {
-    class UC1
+    class Program
     {
         public static void Main(string[] args)
         {
             const int IS_FULL_TIME = 1;
+            const int RATE_PER_HR = 20;
+            int empHrs;
+            int empWage;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("EMPLOYEE IS PRESENT");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("EMPLOYEE IS ABSENT");
+                empHrs = 0;
             }
-
+            empWage = empHrs * RATE_PER_HR;
+            Console.WriteLine("Employee wage is:" + empWage);
         }
     }
 }
+
