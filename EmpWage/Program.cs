4610d70b9ee810f34d453256cee7ce484f8055a1
unknown <hindhujhasridhar@gmail.com>
2021-11-25 22:02:41 +0530

    Switch Case Is Used

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,31 +1,35 @@
-﻿
-//To calculate Both part time and full time wage
-using System;
+﻿using System;
 namespace EmpWage1
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 0;
+
+
             const int rate_per_hr = 20;
-            int part_time_empHrs = 0;
+
             int full_time_empHrs = 0;
+            int part_time_empHrs = 0;
             int full_time_empWage;
             int part_time_empWage;
-            
+
 
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME)
+
+            switch (empCheck)
             {
-                full_time_empHrs = 16;
-                part_time_empHrs = 8;
+                case 0:
+                    full_time_empHrs = 16;
+                    break;
+                case 1:
+                    part_time_empHrs = 8;
+                    break;
+                default:
+                    break;
 
             }
-            
-
             full_time_empWage = rate_per_hr * full_time_empHrs;
             part_time_empWage = rate_per_hr * part_time_empHrs;
             Console.WriteLine("FULL TIME EMPLOYEE WAGE IS:" + full_time_empWage);
@@ -34,6 +38,5 @@ namespace EmpWage1
         }
     }
 }
-
 
 
